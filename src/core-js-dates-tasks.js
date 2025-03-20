41383ba11a00927047e51a71993df625b219d80f
zzzhuchok <zzzhuchok@gmail.com>
2025-03-20 17:13:58 +0300

    feat: add a solution 1-5

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return new Date(date).getTime();
 }
 
 /**
@@ -31,8 +31,15 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  if (!(date instanceof Date) || Number.isNaN(date)) {
+    throw new Error('Invalid Date object');
+  }
+  const hour = `${date.getHours()}`.padStart(2, 0);
+  const min = `${date.getMinutes()}`.padStart(2, 0);
+  const sec = `${date.getSeconds()}`.padStart(2, 0);
+
+  return `${hour}:${min}:${sec}`;
 }
 
 /**
@@ -46,8 +53,9 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dayOfWeek = new Date(date).toLocaleString('en-US', { weekday: 'long' });
+  return dayOfWeek;
 }
 
 /**
@@ -61,8 +69,15 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const targetDay = 5;
+  const currentDay = date.getDay();
+  const diff = targetDay - currentDay;
+  const addDays = diff > 0 ? diff : diff + 7;
+  const nextFriday = new Date(date);
+
+  nextFriday.setDate(nextFriday.getDate() + addDays);
+  return nextFriday;
 }
 
 /**
@@ -76,8 +91,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
